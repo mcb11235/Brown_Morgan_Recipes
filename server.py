d225bfdd44d9268b858e829fc96725146ee1328b
Morgan Brown <mcbrowm1890@gmail.com>
2022-12-21 12:14:58 -0500

    add project model and controller

--- server.py
+++ server.py
@@ -4,5 +4,7 @@ from flask_app.controllers import users
 from flask_app.models.post import Equipment
 from flask_app.controllers import posts
 from flask_app.controllers import dashboard
+from flask_app.models.project import Project
+from flask_app.controllers import projects
 if __name__ == "__main__":
     app.run(debug=True)
